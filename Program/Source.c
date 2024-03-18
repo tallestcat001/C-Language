178630cbcc74fa1a4db465434f433530876fbf70
tallestcat001 <shdmlgus123_@naver.com>
2024-03-18 19:44:38 +0900

    (2024.3.18) 부동 소수점 오차

--- Program/Source.c
+++ Program/Source.c
@@ -1,94 +1,61 @@
 #include <stdio.h>
 void main()
 {
-#pragma region 비트
 
-	// 데이터를 나타내는 최소의 단위이며, 0 또는 1의 조합으로 논리 계산을 수행하는 단위.
+#pragma region 오버플로우
 
-#pragma region 10 진수를 2 진수로 변환하는 과정
+	// 특정한 자료형이 표현할 수 있는 최댓값의 범위를 넘어서 연산을 수행하는 과정.
 
-	// 10 진수를 1이 될 때까지 계속 2로 나누어 준 다음 나눈 위치의 나머지 값을 아래에서 위로 순서대로 정렬함.
+	// char data = 128;
+	// printf("data 변수의 값은 : %d\n", data);
 
-	// 15 -> 2 진수 (1111)
-
-#pragma endregion
-
-#pragma region 2 진수를 10 진수로 변환하는 과정
-
-	// 1 byte에 2 진수로 저장된 값을 2의 제곱으로 나타냄.
-
-	// 각각의 비트에 1이 있다면 1과 2의 제곱의 위치를 계산한 다음 각각의 비트를 모두 더하여 10 진수로 나타냄.
-		
-#pragma endregion
-
-	// 메모리는 비트 단위로 데이터를 저장할 수 있으며, 1개의 비트에는 0 또는 1의 값만 저장할 수 있음.
-	
-#pragma endregion
-
-#pragma region  비트 연산자
-
-	// 비트 단위로 논리 연산을 수행하기 위해 사용하는 연산자.
-
-	// char x = 10; // 0000 1010
-	// char y = 6; // 0000 0110
-	 
-	// printf("x 변수와 y 변수를 AND 연산한 결과 : %d\n", x & y);
-
-	// AND 연산자는 두 개의 피연산자가 모두 1이면 1을 반환하는 연산자.
-
-	// char n = 12; // 0000 1100
-	// char m = 3; // 0000 0011
- 
-	// printf("n 변수와 m 변수를 OR 연산한 결과 : %d\n", n | m);
-	
-	// OR 연산자는 두 개의 피연산자 중에 하나라도 1이 있다면, 1을 반환하는 연산자.
-	
-	// char w = 11; // 0000 1011
-	// char v = 5; // 0000  0101
-	
-	// printf("w 변수와 v 변수를 XOR 연산한 결과 : %d\n", w ^ v);
-	
-	// OR 연산자는 두 개의 피연산자가 서로 같으면 0을 반환하고, 서로 다르면 1을 반환하는 연산자.
-	
-	// char k = 9; // 0000 1001 -> 1111 0110
-	
-	// printf("k 변수를 NOT 연산한 결과 : %d\n", ~k);
-	
-	// 첫 번째 비트는 부호는 나타내며, 첫 번째 비트에 1이 있다면, 음수가 됨.
+	// 최댓값보다 더 많은 값을 저장하게 되면 최솟값부터 최댓값을 넘어간 만큼 다시 계산.
 
 #pragma endregion
 
-#pragma region  시프트 연산자
+#pragma region 언더플로우
 
-	// 각각의 비트를 오른쪽 또는 왼족으로 한 칸 씩 이동시키는 연산자.
+	// 특정 자료형이 표현할 수 있는 최솟값의 범위를 넘어서 연산을 수행하는 과정.
 
-	// int value = 3; // 0000 0011
+	// char data = -129;
+	// printf("data 변수의 값은 : %d\n", data);
 
-	// printf("value 변수를 왼쪽으로 3번 시프트 연산한 결과 : %d\n", value << 3); // 0001 1000
- 
-	// printf("value 변수를 오른쪽으로 1번 시프트 연산한 결과 : %d\n", value >> 1); // 0000 0001
-	
-
+	// 최솟값보다 더 작은 값을 저장하게 되면 최댓값부터 최솟값을 넘어간 만큼 다시 계산.
 
 #pragma endregion
 
-#pragma region 산술 연산자
+#pragma region 부호없는 자료형
 
-	// 1. int (result1) 변수 = 심볼릭 상수 + 리터럴 상수
-	// 2. int (result2) 변수 = 리터럴 상수 - 리터럴 상수
-	// 3. int (result3) 변수 = 변수 * 심볼릭 상수
-	// 4. int (result4) 변수 = 변수 / 리터럴 상수
+	// unsigned int exp = 4294967295;
+	// unsigned int count = -1;
 
-	const int i = 2;
-	int j = 14;
+	// printf("exp 변수의 값은 : %u\n", exp);
+	// printf("count 변수의 값은 : %u\n", count);
 
-	
-	printf("result1 : %d\n", i + 3);
-	printf("result2 : %d\n", 15 - 7);
-	printf("result3 : %d\n", 14 * i);
-	printf("result4 : %d\n", 14 / 2);
+	// 부호없는 자료형에서도 똑같이 오버플로우와 언더플로우가 발생함.
 
 #pragma endregion
+
+#pragma region 실수 형태의 10 진수를 2 진수로 변환하는 과정
+
+	// 10 진수  실수 부분을 1.0이 될 때까지 계속 2로 곱한 다음 곱한 결과의 정수 부분을 위에서 아래로 순서대로 정렬.
+
+	// float x = 13.625f;
+	// printf("x 변수의 값은 : %f\n", x);
+
+#pragma endregion
+
+#pragma region 부동소수점
+
+	// 소수점의 위치를 고정하기 않고, 소수점의 위치를 나타내는 방법.
+
+	// float decimal = 10.3333333f;
+	// printf("decimal 변수의 값은 : %.7f\n", decimal);
+
+	// 가수부에 있는 비트 중에서 가장 왼쪽에 있는 수는 표현하지 않음.
+
+#pragma endregion
+
 
 
 }	
