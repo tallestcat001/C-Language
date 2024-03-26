0255522fc06431d65ac18c2e9c830655c174f0d7
tallestcat001 <shdmlgus123_@naver.com>
2024-03-26 19:42:13 +0900

    (2024.3.26) 문자열

---
+++ Class9th (Arrangement)/Source.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+void main()
+{
+
+#pragma region Scanf() 함수
+
+	// 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에 마추어 입력해주는 함수.
+
+	// int data = 0;
+	// printf("data 변수의 값을 입력 : ");
+	// scanf_s("%d", &data);
+
+	// printf("data 변수의 값은 : %d\n", data);
+
+	// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에 데이터를 보관하였다가 입력되는 순간 버퍼 안의 내용을 프로그램에 전송함.
+
+	//	int count = 5;
+
+	//	while (count > 0)
+	//	{
+	//		printf("count 변수의 값 입력 : ");
+	//		scanf_s("%d", &count);
+
+	//		printf("count 변수의 값 : %d\n", count);
+	//	}
+
+	// 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로 넘어갈 수 없으며, 
+	// 버퍼는 데이터가 이동할 때 임시로 저장되는 공간이며, 
+	// 서식 지정자의 따라 입력할 수 있는 데이터의 범위가 결정됨.
+
+#pragma endregion
+
+#pragma region	배열
+
+	// 같은 자료형의 변수들로 이루어진 유한 집합.
+
+	//	int array[5];
+
+	//	for (int i = 0; i < 5; i++)
+	//	{
+	//		array[i] = (i+1) * 10;
+	//		printf("array[%d]의 값은 : %d\n", i, array[i]);
+	//	}
+
+	// 배열의 경우 첫번째 원소는 0부터 시작함.
+
+	// int size = 5; // 크기 고정 X.
+
+	// int room[5]; // 크기 고정.
+
+	// 배열은 원하는 원소에 값을 저장할 수 있으며, 
+	// 배열의 크기는 컴파일이 되는 시점부터 고정된 메모리 공간을 가지게 됨.
+
+	// char string[] = { 'A','B','C','D','E','F' };
+
+	// printf("string 배열의 크기는 : %d\n", sizeof(string));
+
+	// 배열의 크기는 생략할 수 있으며, 
+	// 초기화 목록에서 설정한 요소에 다라 배열의 크기가 결정됨.
+
+	// int list[5];
+
+	// printf("list의 주소는 : %p\n", list);
+	// printf("list[0]의 주소는 : %p\n", &list[0]);
+
+	// int* ptr = NULL;
+
+	// ptr = list;
+
+	// *ptr = 10;
+
+	// printf("list[0]의 값은 : %d\n", list[0]);
+
+	// ptr = ptr + 1;
+
+	// *ptr = 20;
+
+	// printf("list[1]의 값은 : %d\n", list[1]);
+
+	// 배열은 연속적인 메모리 공간을 가지며, 배열의 이름은 배열의 시작 주소를 가리킴.
+
+#pragma endregion
+
+}
--- Program/Source.c
+++ Program/Source.c
@@ -1,85 +1,92 @@
 #include <stdio.h>
 void main()
 {
-	
-#pragma region Scanf() 함수
 
-	// 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에 마추어 입력해주는 함수.
+#pragma region 문자열
 
-	// int data = 0;
-	// printf("data 변수의 값을 입력 : ");
-	// scanf_s("%d", &data);
+	// 연속적인 메모리 공간에 저장된 문자 변수의 집합.
+
+	// const char* ptr = "Game";
+
+	// *ptr = 'T'; // (역참조 X)
+
+	// printf("ptr이 가리키는 값은 : %s\n", ptr); // %s : 문자열을 출력하는 서식 지정자.
+
+	// ptr = "School";
+
+	// printf("ptr이 가리키는 값은 : %s\n", ptr);
+
+	// 문자열에 경우 포인터를 이용하여 문자열 상수를 가리키도록 할 수 있으며, 
+	// 문자열 상수는 데이터 영역의 읽기 전용 공간에 저장되기 때문에 문자열의 값을 저장할 수 없음.
+
+	// char string[] = "Control";
+
+	// printf("string의 크기는 : %d\n", sizeof(string));
+
+	// 문자열은 공백도 함께 메모리 공간에 포함하여 크기를 결정하며,
+	// 마지막에 문자열의 끝을 알려주는 제어 문자가 추가됨.
+
+	 // char name[] = "Ba\0rd"; // \0 : 배열 사이의 null문자를 추가함.
  
-	// printf("data 변수의 값은 : %d\n", data);
+	 // name[0] = 'W';
+ 
+	 // name = "Alistar"; // 새로운 값 (X)
+ 
+	 // printf("name배열의 값은 : %s\n", name);
 
-	// 표준 입력 함수로 데이터를 입력하게 되면 버퍼에 데이터를 보관하였다가 입력되는 순간 버퍼 안의 내용을 프로그램에 전송함.
-
-	//	int count = 5;
-	
-	//	while (count > 0)
-	//	{
-	//		printf("count 변수의 값 입력 : ");
-	//		scanf_s("%d", &count);
-	
-	//		printf("count 변수의 값 : %d\n", count);
-	//	}
-
-	// 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로 넘어갈 수 없으며, 
-	// 버퍼는 데이터가 이동할 때 임시로 저장되는 공간이며, 
-	// 서식 지정자의 따라 입력할 수 있는 데이터의 범위가 결정됨.
+	// 문자열의 경우 서로 연속적인 메모리 공간으로 연결되어 있지만, 
+	// 문자 배열 사이에 무효의 문자를 넣게 되면 무효의 문자까지만 문자열을 출력함.
 
 #pragma endregion
 
-#pragma region	배열
+#pragma region 약수구하기
 
-	// 같은 자료형의 변수들로 이루어진 유한 집합.
-
-	//	int array[5];
-	
-	//	for (int i = 0; i < 5; i++)
+	//	int a = 0;
+	//	
+	//	printf("정수값을 입력하세요 : ");
+	//	scanf_s("%d", &a);
+	//	
+	//	printf("%d의 약수는 : ", a);
+	//	
+	//	for (int i = 1; i <= a; i++)
 	//	{
-	//		array[i] = (i+1) * 10;
-	//		printf("array[%d]의 값은 : %d\n", i, array[i]);
+	//		if (a % i == 0)
+	//		{
+	//			printf("%d ", i);
+	//		}
 	//	}
-
-	// 배열의 경우 첫번째 원소는 0부터 시작함.
-
-	// int size = 5; // 크기 고정 X.
-
-	// int room[5]; // 크기 고정.
-
-	// 배열은 원하는 원소에 값을 저장할 수 있으며, 
-	// 배열의 크기는 컴파일이 되는 시점부터 고정된 메모리 공간을 가지게 됨.
-
-	// char string[] = { 'A','B','C','D','E','F' };
-
-	// printf("string 배열의 크기는 : %d\n", sizeof(string));
-
-	// 배열의 크기는 생략할 수 있으며, 
-	// 초기화 목록에서 설정한 요소에 다라 배열의 크기가 결정됨.
-
-	// int list[5];
- 
-	// printf("list의 주소는 : %p\n", list);
-	// printf("list[0]의 주소는 : %p\n", &list[0]);
- 
-	// int* ptr = NULL;
- 
-	// ptr = list;
-
-	// *ptr = 10;
- 
-	// printf("list[0]의 값은 : %d\n", list[0]);
- 
-	// ptr = ptr + 1;
- 
-	// *ptr = 20;
- 
-	// printf("list[1]의 값은 : %d\n", list[1]);
-
-	// 배열은 연속적인 메모리 공간을 가지며, 배열의 이름은 배열의 시작 주소를 가리킴.
+	//	printf("\n");
 
 #pragma endregion
 
+#pragma region 범용 포인터
+
+	// 자료형이 정해지지않은 상태로 모든 자료형을 저장할 수 있는 포인터.
+
+	// int data = 100;
+	// float health = 99.75f;
+	// void* vptr = &data;
+ 
+	// *vptr = 1000; // 기본 역참조 (X)
+ 
+	// 범용 포인터는 메모리 주소에 접근해서 값을 변경할 수 없음.
+
+	// *(int*)vptr = 1000;
+ 
+	// printf("data의 값은 : %d\n", data);
+ 
+	// vptr = &health;
+ 
+	// *(float*)vptr = 100.0f;
+ 
+	// printf("health의 값은 : %f\n", health);
+
+	// vptr = "Visual Stdio";
+
+	// printf("vptr의 값은 : %s\n", vptr);
+
+	// 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가 가리키는 변수의 자료형으로 잘료형 변환을 해주어야함.
+
+#pragma endregion
 
 }	
