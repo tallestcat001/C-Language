9832b68ae0ddd60278765bde93a84863ff02da35
tallestcat001 <shdmlgus123_@naver.com>
2024-03-28 19:49:14 +0900

    (2024.3.28) 재귀함수

---
+++ Class11th (Function)/Source.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+void function()
+{
+	printf("function\n");
+}
+
+void damage(int attack)
+{
+	printf("%d의 데미지를 받았습니다.\n", attack);
+}
+
+float add(float x, float y)
+{
+	return x + y;
+}
+
+void recovery(float health)
+{
+	health = 100;
+}
+
+void swap(int* x, int* y)
+{
+	int z;
+
+	z = *y;
+	*y = *x;
+	*x = z;
+}
+
+int main()
+{
+
+#pragma region 함수
+
+	// 하나의 특별한 목적의 작업을 수행하기 위해 독립적으로 설계된 코드의 집합.
+
+	// function();
+
+	// 함수의 경우 자료형과 반환하는 값의 형태가 일치하지 않으면 원하는 값을 얻을 수 없음.
+
+#pragma endregion
+
+#pragma region 매개변수
+
+	// 함수의 정의에서 전달받은 임수를 함수 내부로 전달하기 위해 사용하는 변수.
+
+	// damage(25);
+
+	// 매개변수는 함수 내부에서만 연산이 이루어지며, 함수가 종료되면 메모리에서 사라지는 특징을 가짐.
+
+	// printf("add(10.5f, 20.75f)의 값은 : %f\n", add(10.5f, 20.75f));
+
+	// 하나의 함수에 서로 다른 자료형의 매개변수를 함께 정의할 수 있으며, 여러 개의 매개변수 선언할 수 있음.
+
+#pragma endregion
+
+#pragma region 인수
+
+	// 함수가 호출될 때 매개변수에 실제로 전달되는 값.
+
+	// int health = 50;
+
+	// recovery(health);
+
+	// printf("health변수의 값은 : %d\n", health);
+
+	// 함수를 호출할 때 변수의 값을 인수로 전달하게 되면 함수의 매개변수에 변수의 복사된 값이 전달되기 때문에 
+	// 인수로 전달된 변수는 함수 내에서 일어난 연산에 대해 영향을 받지 않음.
+
+	// int a = 10;
+	// int b = 20;
+
+	// printf("a 변수의 값은 : %d\n", a);
+	// printf("b 변수의 값은 : %d\n", b);
+	// printf("\n");
+
+	// swap(&a,&b);
+
+	// printf("a 변수의 값은 : %d\n", a);
+	// printf("b 변수의 값은 : %d\n", b);
+
+	// 함수를 호출할 때 변수의 주소를 인수로 전달하게 되면 전달한 변수의 주소가 참조되어 
+	// 인수로 전달한 변수의 값이 함수 내에서 일어난 연산에 대해 영향을 받음.
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Source.c
+++ Program/Source.c
@@ -1,90 +1,107 @@
 #include <stdio.h>
-void function()
+void awake()
 {
-	printf("function\n");
-}
+	printf("awake\n");
 
-void damage(int attack)
+	awake();
+}
+void recursive(int count)
 {
-	printf("%d의 데미지를 받았습니다.\n", attack);
-}
+	if (count <= 0)
+	{
+		return;
+	}
 
-float add(float x, float y)
+	recursive(count - 1);
+
+	printf("count 변수의 값은 : %d\n", count);
+
+}
+inline void process(int data)
 {
-	return x + y;
-}
+	printf("%d%%만큼 데이터가 처리되었습니다.\n", data);
 
-void recovery(float health)
-{
-	health = 100;
-}
+	// 인라인 함수는 컴파일 시점에 확장되며, 컴파일시 온라인 함수로 선언하더라도 상황에 따라 일반 함수로 변환되기도 함.
 
-void swap(int* x, int* y)
-{
-	int z;
-	
-	z = *y;
-	*y = *x;
-	*x = z;
 }
-
 int main()
 {
 
-#pragma region 함수
+#pragma region 재귀함수
 
-	// 하나의 특별한 목적의 작업을 수행하기 위해 독립적으로 설계된 코드의 집합.
+	// 어떤 함수에서 자신을 다시 호출하여 작업을 수행하는 함수.
 
-	// function();
+	// awake();
 
-	// 함수의 경우 자료형과 반환하는 값의 형태가 일치하지 않으면 원하는 값을 얻을 수 없음.
+	// 재귀함수는 함수를 계속 호출하기 때문에 스택영역에 메모리가 계속 쌓이게 되므로, 스택 오버플로우가 발생함.
 
-#pragma endregion
+	// recursive(3);
 
-#pragma region 매개변수
-
-	// 함수의 정의에서 전달받은 임수를 함수 내부로 전달하기 위해 사용하는 변수.
-
-	// damage(25);
-
-	// 매개변수는 함수 내부에서만 연산이 이루어지며, 함수가 종료되면 메모리에서 사라지는 특징을 가짐.
-
-	// printf("add(10.5f, 20.75f)의 값은 : %f\n", add(10.5f, 20.75f));
-
-	// 하나의 함수에 서로 다른 자료형의 매개변수를 함께 정의할 수 있으며, 여러 개의 매개변수 선언할 수 있음.
+	// 재귀함수의 경우 특정한 시점에서 함수를 반환해야 하며, 
+	// 재귀적으로 호출한 함수는 스택 프레임에 의해 마지막에 호출된 함수부터 차례대로 스택 영역에서 해제됨.
 
 #pragma endregion
 
-#pragma region 인수
+#pragma region 최댓값과 최소값
 
-	// 함수가 호출될 때 매개변수에 실제로 전달되는 값.
+	//	int array[5] = { 6,11,1,2,8 };
+	//	int max = array[0];
+	//	int min = array[0];
+	//	
+	//	for (int i = 0; i < 5; i++)
+	//	{
+	//		if (max < array[i])
+	//		{
+	//			max = array[i];
+	//		}
+	//		if (min > array[i])
+	//		{
+	//			min = array[i];
+	//		}
+	//	}
+	//	
+	//	printf("최댓값은 : %d\n", max);
+	//	printf("최소값은 : %d\n", min);
+	
+#pragma endregion
 
-	// int health = 50;
+#pragma region 인라인 함수
 
-	// recovery(health);
+	// 함수를 호출하는 대신 함수가 호출되는 위치마다 함수의 코드를 복사하여 전달하는 방식의 함수.
 
-	// printf("health변수의 값은 : %d\n", health);
+	// process(75);
 
-	// 함수를 호출할 때 변수의 값을 인수로 전달하게 되면 함수의 매개변수에 변수의 복사된 값이 전달되기 때문에 
-	// 인수로 전달된 변수는 함수 내에서 일어난 연산에 대해 영향을 받지 않음.
+	// 인라인 함수는 함수를 호출하는 과정이 없으므로 처리속도가 빠르지만,
+	// 인라인 함수를 많이 사용하게 하게 되면 함수의 코드가 복사되기 때문에 실행파일의 크기가 커지게됨.
 
-	// int a = 10;
-	// int b = 20;
- 
-	// printf("a 변수의 값은 : %d\n", a);
-	// printf("b 변수의 값은 : %d\n", b);
-	// printf("\n");
+#pragma endregion
 
-	// swap(&a,&b);
+#pragma region 소수
 
-	// printf("a 변수의 값은 : %d\n", a);
-	// printf("b 변수의 값은 : %d\n", b);
+	int a = 0;
+	int b = 0;
 
-	// 함수를 호출할 때 변수의 주소를 인수로 전달하게 되면 전달한 변수의 주소가 참조되어 
-	// 인수로 전달한 변수의 값이 함수 내에서 일어난 연산에 대해 영향을 받음.
+	printf("정수를 입력하세요 : ");
+	scanf_s("%d", &a);
+
+	for (int i = 2; i <= a; i++)
+	{
+		if (a % i == 0)
+		{
+			b++;
+		}
+	}
+
+	if (b == 1)
+	{
+		printf("%d의 값은 소수입니다.\n", a);
+	}
+	else
+	{
+		printf("%d의 값은 소수가 아닙니다.\n", a);
+	}
 
 #pragma endregion
 
 	return 0;
-
 }	
