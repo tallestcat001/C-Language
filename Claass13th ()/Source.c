1085deaae10be2978f81f30faaae6c3f36d56135
tallestcat001 <shdmlgus123_@naver.com>
2024-04-01 19:45:44 +0900

    (2024.4.1) 동적 할당

---
+++ Claass13th ()/Source.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+int itemcode;
+int level = 1;
+void stage()
+{
+	level += 1;
+}
+void score()
+{
+	// 정적변수는 단 한번만 초기화가 이루어지는 변수.
+	static int score = 0;
+
+	score++;
+
+	printf("score 변수의 값은 : %d\n", score);
+}
+int main()
+{
+
+#pragma region 지역변수
+
+	// 함수 내부에서 선언된 변수로 { } 내에서만 접근할 수 있으며, 
+	// { }를 벗어나게 되면 메모리에서 사라지는 특징을 가지고 있음.
+
+	//	int data = 100;
+
+	//	{
+	//		int data = 200;
+
+	//		printf("data 변수의 값은 : %d\n", data); // 변수의 범위 규칙 : 가장 가까운 위치에 있는 data 변수의 접근.
+	//	}
+
+	//	printf("data 변수의 값은 : %d\n", data);
+
+#pragma endregion
+
+#pragma region 전역변수
+
+	// 함수 외부에 선언된 변수로 어디에서든지 접근이 가능하며, 
+	// 프로그램이 실행될 때 생성되고 프로그램이 종료될 때 메모리에서 해제되는 특징을 가짐.
+
+	// stage();
+
+	// printf("level 변수의 값은 : %d\n", level);
+
+	// printf("itemcode 변수의 값은 : %d\n", itemcode);
+
+#pragma endregion
+
+#pragma region 정적변수
+
+	// static이라는 키워드를 사용하고 지역변수의 특징을 가지면서, 전역변수의 특징을 가지고 있는 변수.
+
+	// score();
+	// score();
+	// score();
+
+#pragma endregion
+
+#pragma region 소인수분해
+
+	//	int a;
+	//	printf("정수를 입력해주세요 : ");
+	//	scanf_s("%d", &a);
+
+	//	for (int i = 2; i <= a; i++)
+	//	{
+	//		if (a % i == 0)
+	//		{
+	//			a /= i;
+	//			printf("%d ", i);
+	//			i--;
+	//		}
+	//	}
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Source.c
+++ Program/Source.c
@@ -1,77 +1,84 @@
 #include <stdio.h>
-int itemcode;
-int level = 1;
-void stage() 
-{
-	level += 1;
-}
-void score()
-{
-	// 정적변수는 단 한번만 초기화가 이루어지는 변수.
-	static int score = 0;
-
-	score++;
-
-	printf("score 변수의 값은 : %d\n", score);
-}
+#include <stdlib.h>
 int main()
 {
 
-#pragma region 지역변수
+#pragma region 동적 할당
 
-	// 함수 내부에서 선언된 변수로 { } 내에서만 접근할 수 있으며, 
-	// { }를 벗어나게 되면 메모리에서 사라지는 특징을 가지고 있음.
+	// 프로그램을 실행중에 필요한 만큼 메모리를 할당하는 작업.
 
-	//	int data = 100;
-	
-	//	{
-	//		int data = 200;
-	
-	//		printf("data 변수의 값은 : %d\n", data); // 변수의 범위 규칙 : 가장 가까운 위치에 있는 data 변수의 접근.
-	//	}
-	
-	//	printf("data 변수의 값은 : %d\n", data);
+	// int* ptr = (int *)malloc(sizeof(int));
 
-#pragma endregion
+	// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를 변경시킬 수 있으며, 
+	// 동적으로 메모리를 크기를 할당할 때 바이트 단위로 반환.
 
-#pragma region 전역변수
+	// printf("ptr이 가리키는 값은 : %d\n", *ptr);
 
-	// 함수 외부에 선언된 변수로 어디에서든지 접근이 가능하며, 
-	// 프로그램이 실행될 때 생성되고 프로그램이 종료될 때 메모리에서 해제되는 특징을 가짐.
+	// *ptr = 100;
 
-	// stage();
+	// printf("ptr이 가리키는 값은 : %d\n", *ptr);
 
-	// printf("level 변수의 값은 : %d\n", level);
+	// 메모리를 동적 할당할 때 주조를 범용 포인터로 반환하기 때문에
+	//  자료형을 반환한 다음 메모리에 할당해야함.
 
-	// printf("itemcode 변수의 값은 : %d\n", itemcode);
+	// free(ptr);
+	// free(ptr); // 2번 사용시 오류발생.
+
+	// 동적으로 할당한 메모리는 힙 영역에 보관되어 있으므로, 
+	// 사용이 끝나면 직접 해제해주어야함.
 
 #pragma endregion
 
-#pragma region 정적변수
+#pragma region 댕글링 포인터
 
-	// static이라는 키워드를 사용하고 지역변수의 특징을 가지면서, 전역변수의 특징을 가지고 있는 변수.
+	// 이미 해제된 메모리 영역을 가리키는 포인터.
 
-	// score();
-	// score();
-	// score();
+	// int* ptr1 = malloc(4);
+
+	// *ptr1 = 100;
+ 
+	// printf("ptr1이 가르키는 값은 : %d\n", *ptr1);
+
+	// free(ptr1);
+
+	// ptr1 = NULL;
+
+	// *ptr1 = 999; // 해제된 메모리 공간에 접근하여 값을 저장.
+ 
+	// printf("ptr1이 가르키는 값은 : %d\n", *ptr1);
 
 #pragma endregion
 
-#pragma region 소인수분해
+#pragma region 동적 배열
 
-	//	int a;
-	//	printf("정수를 입력해주세요 : ");
-	//	scanf_s("%d", &a);
-	//	
-	//	for (int i = 2; i <= a; i++)
-	//	{
-	//		if (a % i == 0)
-	//		{
-	//			a /= i;
-	//			printf("%d ", i);
-	//			i--;
-	//		}
-	//	}
+	// int dynamicarraysize = 5;
+ 
+	// int* arrptr = (int*)malloc(sizeof(int) * dynamicarraysize);
+ 
+	// int size = (sizeof(int) * dynamicarraysize) / sizeof(int);
+
+	// for (int i = 0; i < size; i++)
+	// {
+	// 	arrptr[i] = i + 1;
+	// 	printf("arrptr[%d] : %d\n", i, arrptr[i]);
+	// } 
+	// free(arrptr);
+
+	// dynamicarraysize = 10;
+
+	// arrptr = (int*)malloc(sizeof(int) * dynamicarraysize);
+
+	// size = (sizeof(int) * dynamicarraysize) / sizeof(int);
+
+	// printf("\n");
+
+	// for (int i = 0; i < size; i++)
+	// {
+	// 	arrptr[i] = i + 1;
+	// 	printf("arrptr[%d] : %d\n", i, arrptr[i]);
+	// }
+ 
+	// free(arrptr);
 
 #pragma endregion
 
