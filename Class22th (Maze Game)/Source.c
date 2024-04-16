0aa2eb237cc17ac11f10d1f70907f5cd6f99f937
tallestcat001 <shdmlgus123_@naver.com>
2024-04-16 19:46:34 +0900

    (2024.4.16) 미로 찾기 게임 2

---
+++ Class22th (Maze Game)/Source.c
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../Program/keyboard.h"
+
+#define width 11
+#define height 11
+
+char maze[width][height];
+
+void init()
+{
+	strcpy(maze[0], "1111111111");
+	strcpy(maze[1], "1001111111");
+	strcpy(maze[2], "1101111111");
+	strcpy(maze[3], "1100000111");
+	strcpy(maze[4], "1111110111");
+	strcpy(maze[5], "1111110111");
+	strcpy(maze[6], "1111110111");
+	strcpy(maze[7], "1111110111");
+	strcpy(maze[8], "1111110111");
+	strcpy(maze[9], "1111110021");
+	strcpy(maze[10], "1111111111");
+}
+void randerer()
+{
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 11; j++)
+		{
+			if (maze[i][j] == '1')
+			{
+				printf("¡á");
+			}
+			else if (maze[i][j] == '0')
+			{
+				printf(" ");
+			}
+			else if (maze[i][j] == '2')
+			{
+				printf("¡Ý");
+			}
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	Character character = { 3,1,"¡Ú" };
+
+	init();
+	while (1)
+	{
+		randerer();
+
+		input(maze, &character);
+		gotoXY(character.x, character.y);
+
+		printf("%s", character.shape);
+
+		Sleep(50);
+
+		system("cls");
+	}
+
+	return 0;
+}
--- Program/keyboard.h
+++ Program/keyboard.h
@@ -1,4 +1,6 @@
+#pragma once
 #include "character.h"
+#include <conio.h>
 
 #define UP 72
 #define LEFT 75
@@ -11,4 +13,28 @@
 void input(char maze[width][height], Character* character)
 {
 
+	if (maze[character->x][character->y] == 2)
+	{
+		exit(1);
+	}
+
+	if (_kbhit())
+	{
+		char key = _getch();
+		
+
+		switch (key)
+		{
+		case 32 : 
+			break;
+		case UP: if (maze[character->y - 1][character->x / 2] != '1')character->y--;
+			break;
+		case LEFT: if (maze[character->y][character->x / 2 - 1] != '1')character->x -= 2;
+			break;
+		case RIGHT: if (maze[character->y][character->x / 2 + 1] != '1')character->x += 2;
+			break;
+		case DOWN: if (maze[character->y + 1][character->x / 2] != '1')character->y++;
+			break;
+		}
+	}
 }
